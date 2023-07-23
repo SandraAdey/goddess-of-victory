23b05c30c877836b08fc7feb6bb91a129c55b2f9
Sandra Adey <sandraadeyinka@gmail.com>
2023-07-24 00:35:25 +0300

    background img

--- main.py
+++ main.py
@@ -7,3 +7,27 @@ pygame.init()
 #create game window
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Brawler")
+
+#loading back ground image
+bg_image = pygame.image.load("image/nikke.png").convert_alpha()
+
+def draw_bg():
+  screen.blit(bg_image, (0, 0))
+
+#game loop
+run = True
+while run:
+  
+  draw_bg()
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+
+  pygame.display.update()     
+
+pygame.quit()
